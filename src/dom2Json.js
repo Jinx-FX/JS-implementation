0f0b08b05a9496e493af301c2cae7e235666824f
Jinx-FX <2674802603@qq.com>
2023-05-25 23:19:15 +0800

    fix: attrs add

--- src/dom2Json.js
+++ src/dom2Json.js
@@ -3,13 +3,19 @@
 
 // 将真实dom转化为虚拟dom
 function dom2Json (dom) {
-  if (!dom.tagName) return;
-  let obj = {};
-  obj.tag = dom.tagName;
-  obj.children = [];
+  if (!dom.tagName) return
+  let obj = {}
+  obj.tag = dom.tagName
+  obj.attrs = {}
+  for (const key in dom.attributes) {
+    if (dom[key].value) {
+      obj.attrs[dom[key].name] = dom[key].value
+    }
+  }
+  obj.children = []
   dom.childNodes.forEach(item => {
     // 去掉空的节点
-    dom2Json(item) && obj.children.push(dom2Json(item));
-  });
-  return obj;
+    dom2Json(item) && obj.children.push(dom2Json(item))
+  })
+  return obj
 }
