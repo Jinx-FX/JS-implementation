0aa8566ae9c0221c442990e36f98caba468fd256
Jinx-FX <2674802603@qq.com>
2023-05-25 18:06:35 +0800

    chore: single.js

--- src/single.js
+++ src/single.js
@@ -5,18 +5,18 @@
 
 class Single {
   constructor(name) {
-    this.name = name;
+    this.name = name
   }
   static getInstance (name) {
     // 静态方法
     if (!this.instance) {
       // 关键代码 this指向的是Single这个构造函数
-      this.instance = new Single(name);
+      this.instance = new Single(name)
     }
-    return this.instance;
+    return this.instance
   }
 }
 
-let single1 = Single.getInstance("name1");
-let single2 = Single.getInstance("name2");
-console.log(single1 === single2);  // true
+let single1 = Single.getInstance("name1")
+let single2 = Single.getInstance("name2")
+console.log(single1 === single2)  // true
