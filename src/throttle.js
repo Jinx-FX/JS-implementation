f8e7bcbe7541523b4bc124e9782dca23bb0803a7
Jinx-FX <2674802603@qq.com>
2023-08-22 14:28:51 +0800

    fix:format

--- src/throttle.js
+++ src/throttle.js
@@ -9,30 +9,30 @@
  * @param {boolean} flag - 第一次是否执行
  */
 function throttle (fn, time, flag) {
-  let timer;
+  let timer
   return function (...args) {
     // flag控制第一次是否立即执行`
     if (flag) {
-      fn.apply(this, args);
+      fn.apply(this, args)
       // 第一次执行完后，flag变为false；否则以后每次都会执行
-      flag = false;
+      flag = false
     }
     if (!timer) {
       timer = setTimeout(() => {
-        fn.apply(this, args);
+        fn.apply(this, args)
         // 每次执行完重置timer
-        timer = null;
-      }, time);
+        timer = null
+      }, time)
     }
-  };
+  }
 }
 
 // 测试
 function fn () {
   console.log(Date.now())
-  console.log("fn");
+  console.log("fn")
 }
-let throttleFn = throttle(fn, 3000, true);
-setInterval(throttleFn, 500);
+let throttleFn = throttle(fn, 3000, true)
+setInterval(throttleFn, 500)
 
 // 测试结果，一开始就打印"fn", 以后每隔3s打印一次"fn"
