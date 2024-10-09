f90dcf2022016b391dd514711c35985d1832e348
StarryRift <77578910+Jinx-FX@users.noreply.github.com>
2024-10-09 21:05:41 +0800

    Update single.js
    使用闭包实现单例模式

--- src/designPatterns/single.js
+++ src/designPatterns/single.js
@@ -20,3 +20,40 @@ class Single {
 let single1 = Single.getInstance("name1")
 let single2 = Single.getInstance("name2")
 console.log(single1 === single2)  // true
+
+// 使用闭包实现单例模式
+const Singleton = (() => {
+  let instance;
+
+  const createInstance = () => {
+    const state = {
+      value: 0,
+    };
+
+    return {
+      getState: () => state,
+      setValue: (newValue) => {
+        state.value = newValue;
+      },
+    };
+  };
+
+  return {
+    getInstance: () => {
+      if (!instance) {
+        instance = createInstance();
+      }
+      return instance;
+    },
+  };
+})();
+
+// 使用单例
+const instance1 = Singleton.getInstance();
+const instance2 = Singleton.getInstance();
+
+instance1.setValue(42);
+
+console.log(instance1.getState().value); // 输出: 42
+console.log(instance2.getState().value); // 输出: 42
+console.log(instance1 === instance2); // 输出: true
